6edb47c9e05edb64c822c0a6c8b2f3dd274bd7e8
Roman Klymashenko <romanklimashenko@gmail.com>
2023-08-27 21:34:51 +0300

    all css is a modules. no css following style

--- config/build/buildLoaders/buildLoaders.ts
+++ config/build/buildLoaders/buildLoaders.ts
@@ -14,7 +14,17 @@ function buildLoaders({ isDev }:BuildOptions): RuleSetRule[] {
     use: [
       isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
       // Translates CSS into CommonJS
-      "css-loader",
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: /.module.scss$/g,
+            localIdentName: isDev
+            ? '[path][name]__[local]--[hash:base64:5]' 
+            : '[hash:base64:8]',
+          },
+        }
+      },
       // Compiles Sass to CSS
       "sass-loader",
     ],
---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,7 @@
+import classes from './Counter.module.scss';
+
+const Counter = () => {
+	return <div className={classes.green}>Counter</div>;
+};
+
+export default Counter;
