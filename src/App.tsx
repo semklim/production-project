1bdd2e3d54165e08290ffeb67cba6dec46535941
Roman Klymashenko <romanklimashenko@gmail.com>
2023-08-28 12:50:42 +0300

    add styles and Loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Suspense, lazy } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
+import Loader from './components/Loader/Loader';
 
 const AboutPage = lazy(() => import('./pages/AboutPage/AboutPage'));
 const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
 
 const App = () => {
 	return (
-		<div className='app'>
+		<div className='app dark'>
 			<Link to={'/'}>Main</Link>
 			<Link to={'/about'}>About</Link>
-			<Suspense fallback={<>Loading ...</>}>
+			<Suspense fallback={<Loader />}>
 				<Routes>
 					<Route path='/about' element={<AboutPage />} />
 					<Route path='/' element={<MainPage />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import './index.scss';
+import './styles/index.scss';
 
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
