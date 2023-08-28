49849047085b87145b6e08bda56a4d7afb7d341d
Roman Klymashenko <romanklimashenko@gmail.com>
2023-08-28 20:37:31 +0300

    add helper classNames

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Suspense, lazy, useContext, useState } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import Loader from './components/Loader/Loader';
 import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames';
 
 const AboutPage = lazy(() => import('./pages/AboutPage/AboutPage'));
 const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
@@ -10,7 +11,7 @@ const App = () => {
 	const { theme, setTheme } = useTheme();
 
 	return (
-		<div className={`app ${theme}`}>
+		<div className={classNames('app', {}, [theme])}>
 			<div className='btn'>
 				<button onClick={setTheme}>Toggle</button>
 			</div>
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,38 @@
+/**
+ * @type Mods - An object containing modifier classes as keys and their corresponding boolean/string values.
+ */
+type Mods = Record<string, boolean | string>;
+
+
+/**
+ * Generates a class name by combining the base class, additional classes, and modified classes.
+ *
+ * @param {string} cls - The base class name.
+ * @param {Mods} [mods={}] - An object containing modifier classes as keys and their corresponding boolean/string values. 
+ * @param {string[]} [additional=[]] - An array of additional class names.
+ * @returns {string} - The generated class name.
+ * 
+ * 
+ *  @example
+ * // Returns: "button primary large"
+ * className("button", { primary: true, large: false }, ["extra"]);
+ * 
+ * @example
+ * // Returns: "card active extra"
+ * className("card", { active: true, extra: true });
+ * 
+ * @example
+ * // Returns: "card"
+ * className("card");
+ */
+export function classNames(cls: string, mods: Mods = {}, additional: string[] = []): string {
+  return [
+
+    cls,
+    ...additional,
+    Object.entries(mods)
+    .filter(([className, value]) => Boolean(value))
+    .map((className) => className)
+
+  ].join(' ');
+}
