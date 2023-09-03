6b50a7f8b02c79b27d7b990e626e6eae880d1060
Roman Klymashenko <romanklimashenko@gmail.com>
2023-09-03 18:11:54 +0300

    finish theme switcher

--- src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,23 +1,20 @@
 import cls from './ThemeSwitcher.module.scss';
 import { classNames } from 'shared/lib';
 import { useTheme } from 'app/provider/theme';
-import { AppButton, ButtonTheme } from 'shared/ui/AppButton/AppButton';
+import { Theme } from 'app/provider/theme/lib/themeContext';
 
 interface ThemeSwitcherProps {
 	className?: string;
 }
 
-const ThemeSwitcher = (props: ThemeSwitcherProps) => {
-	const { setTheme } = useTheme();
-	const { className } = props;
+const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
+	const { theme, setTheme } = useTheme();
+	const checked = theme === Theme.DARK ? true : false;
 	return (
-		<AppButton
-			theme={ButtonTheme.CLEAR}
-			className={classNames(cls.themeSwitcher, {}, [className])}
-			onClick={setTheme}
-		>
-			toggle
-		</AppButton>
+		<label id='switch' className={classNames(cls.switch, {}, [])}>
+			<input type='checkbox' onChange={setTheme} id='slider' checked={checked} />
+			<span className={classNames(cls.slider, {}, [cls.round, className])}></span>
+		</label>
 	);
 };
 
